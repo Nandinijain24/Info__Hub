6d5524aaaf5ff7b06f19d5956519789d26935ba4
Nandinijain23 <Nandinijain23>
2024-11-11 21:01:58 +0530

    commits

--- src/Components/Newsapp.jsx
+++ src/Components/Newsapp.jsx
@@ -1,59 +1,67 @@
-import React, { useEffect,  useState } from 'react'
-import Card from './Card';
+import React, { useEffect, useState } from 'react'
+import Card from './Card'
 
-function Newsapp() {
 
-const [search,setSearch] = useState("India")
-const [newsData,setNewsData] = useState([])
-     const API_KEY ="08d5cda76cb84d149f916a9ae546d706";
+const Newsapp = () => {
+    const [search, setSearch] = useState("india");
+    const [newsData, setNewsData] = useState(null)
+    const API_KEY = "08d5cda76cb84d149f916a9ae546d706";
 
-     const getData =async() =>{
-const response =await fetch(`https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`)
-     const jsonData=await response.json();
-     console.log(jsonData.articles);
-     setNewsData(jsonData.articles)
-}
+    const getData = async() =>{
+        const response = await fetch(`https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`)
+        const jsonData = await response.json();
+        console.log(jsonData.articles);
+        let dt = jsonData.articles.slice(0,10)
+        setNewsData(dt)
+        
+    }
 
-useEffect(()=>{
-getData()
-},[])
-const handleInput=(event)=>{
-     console.log(event.target.value);
-     setSearch(event.target.value)
-}
-const userInput=(event)=>{
-setSearch(event.target.value)
-}
+    useEffect(()=>{
+        getData()
+    },[])
+
+    const handleInput = (e) =>{
+        console.log(e.target.value);
+        setSearch(e.target.value)
+        
+    }
+    const userInput = (event) =>{
+        setSearch(event.target.value)
+    }
+
   return (
     <div>
-<nav>
-     <div>
-          <h1>Trendy News</h1>
-     </div>
-     <ul>
-         <a>All News</a>
-         <a>Trending</a>
-     </ul>
-     <div className='searchBar'>
-          <input type='text' placeholder='Search News' value={search}onChange={handleInput}/>
-          <button onClick={getData}>Search</button>
+        <nav>
+            <div>
+                <h1>Trendy News</h1>
+            </div>
+            <ul style={{display:"flex", gap:"11px"}}>
+                <a style={{fontWeight:600, fontSize:"17px"}}>All News</a>
+                <a style={{fontWeight:600, fontSize:"17px"}}>Trending</a>
 
-     </div>
-</nav>
-<div>
-     <marquee className='head'>Stay update with TrendyNews</marquee>
-</div>
-<div className='categoryBtn'>
-     <button onClick={userInput} value='sports'>Sports</button>
-     <button onClick={userInput} value='politics'>politics</button>
-     <button onClick={userInput}  value='Entertainment'>Entertainment</button>
-     <button onClick={userInput}  value='Health'>Health</button>
-     <button onClick={userInput}  value='Fitness'>Fitness</button>
-</div>
+            </ul>
 
-<div>
-<Card data={newsData}/>
-</div>
+
+            <div className='searchBar'>
+                <input type='text' placeholder='Search News' value={search} onChange={handleInput}/>
+                <button onClick={getData}>Search</button>
+            </div>
+        </nav>
+        <div>
+            <p className='head'>Stay Update with TrendyNews</p>
+        </div>
+        <div className='categoryBtn'>
+            <button onClick={userInput} value="sports">Sports</button>
+            <button onClick={userInput} value="politics">Politics</button>
+            <button onClick={userInput} value="entertainment">Entertainment</button>
+            <button onClick={userInput} value="health">Health</button>
+            <button onClick={userInput} value="fitness">Fitness</button>
+        </div>
+
+        <div>
+        {newsData?  <Card data={newsData}/> : null}
+            
+        </div>
     </div>
   )
 }
